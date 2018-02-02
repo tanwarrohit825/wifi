091725d7934086701a1c15437f97e6e4fd7092d6
Rohit Tanwar <tanwarrohit825@gmail.com>
2018-02-02 22:29:59 +0530

    Initial commit

---
+++ some commend for fidora.sh
@@ -0,0 +1,130 @@
+----------- Some Commend For Fidora -------
+
+
+----------  Change Directory ----------
+
+# change directory
+cd                 
+
+#for help 
+man cd    
+
+ ------ Compleate Commend -------
+
+ #automatic commpleate the commend.
+cd /home/tab , Press tab to complete the commend.  
+
+----------Print Working Directory --------
+
+# print working directory
+pwd         		
+
+--------  List Commend   ------
+
+# list the contect in the current directory.
+ls 				
+
+# pass the argument in ls this show contect in the detail list.
+ls -l 				
+
+# saw the exact size of the folder.
+ls -lh 
+
+# For help in list 
+man ls 	
+
+------ Some Other Commend -------
+
+# Quit
+q 
+
+# Show the all about my pc, software etc.
+uname -a 
+
+# Show kernal 
+uname -r
+
+# Clear the screen.
+ctrl + l or clear  
+
+# Show the commmend history.
+history 		
+
+# Create the file name.
+touch <File name>  or ll <File name >
+
+# Listing a  particular file
+ll <File name>
+
+# move the file
+mv <File name> /Path
+
+# remove the file 
+rm -rf /Path
+
+# see all user in pc 
+cat /etc/passwd
+
+#text editor 
+vi <File name>
+
+# fluch it out contect of the file see all contest in screen
+cat <File name>
+
+
+
+---------  For permission -------
+
+# See this permission
+chmod <Enter Number>
+
+# for help in permission
+man chmod
+
+#removewrite protected file 
+rm /Path
+
+# for help 
+7 = 4+2+1 (read/write/execute)
+6 = 4+2 (read/write)
+5 = 4+1 (read/execute)
+4 = 4 (read)
+3 = 2+1 (write/execute)
+2 = 2 (write)
+1 = 1 (execute)
+
+--------
+#copy of the folder
+cp -Rv <File name>  <copy on the path>
+
+#Pull the things or print 
+ip addr show | grep -i <key word>
+
+------------ About software (all in main admin)---------
+
+# install search  software ------
+dnf search  <software name>
+
+#install of the software 
+dnf install <software name> or dnf install <software name.x64>
+
+#remove the the software 
+dnf remove <software name>
+
+#update the software 
+dnf update
+
+-------------- about precess ---------
+
+#start a particular prosses
+systemctl start <precess name>
+
+# states of the prosses 
+systemctl status <prosses name >
+
+# stop the prosses 
+systemctl stop <prosses name>
+
+
+
+  
